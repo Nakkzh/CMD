d9587c5571e2b9ea851e7ab6d2db25a5502e9aea
Nakkzh <joaoguiguii2310@gmail.com>
2025-02-11 16:54:49 -0300

    Add files via upload

---
+++ CMD.bat
@@ -0,0 +1,14 @@
+REM CRIAR UMA PASTA
+mkdir W
+REM VER OS DIRETORIOS
+dir
+REM IR PARA A PASTA W
+cd W
+REM CRIAR ARQUIVO TXT
+mkdir chat.text
+REM ADICIONAR TEXTOS DENTRO DO AQUIVO TXT
+echo ¯\_(ツ)_/¯ > chat.txt
+echo ( ͡° ͜ʖ ͡°) >> chat.txt
+echo (ง'̀-'́)ง >> chat.txt
+REM VER O QUE TEM NO ARQUIVO DE TEXTO
+type chat.txt
