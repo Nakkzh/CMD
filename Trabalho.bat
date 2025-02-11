d081a7028da054472d39335d4e3e5006c02175a6
Nakkzh <joaoguiguii2310@gmail.com>
2025-02-11 17:01:10 -0300

    Trabalho.bat

--- Trabalho.bat
+++ Trabalho.bat
@@ -5,10 +5,21 @@ dir
 REM IR PARA A PASTA W
 cd W
 REM CRIAR ARQUIVO TXT
-mkdir chat.text
+mkdir chat.txt
 REM ADICIONAR TEXTOS DENTRO DO AQUIVO TXT
 echo ¯\_(ツ)_/¯ > chat.txt
 echo ( ͡° ͜ʖ ͡°) >> chat.txt
 echo (ง'̀-'́)ง >> chat.txt
 REM VER O QUE TEM NO ARQUIVO DE TEXTO
 type chat.txt
+REM VOLTAR O CD
+cd ..
+REM RENOMEAR PASTA
+rename W FIM
+REM CRIAR OUTRA PASTA E MOVER O TXT PARA ELA
+mkdir FIM2
+move FIM\chat.txt FIM2\chat.txt
+REM DELETAR PASTAS COM TUDO DENTRO
+del FIM FIM2
+echo CABO!!!
+pause
